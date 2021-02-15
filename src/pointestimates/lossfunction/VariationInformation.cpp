1c8a0504e91b31862a0cc50211d1690830e997eb
João Oliveira <51805601+JoaoHenriqueOliveira@users.noreply.github.com>
2021-02-15 21:16:33 +0100

    Possible optimization in the Entropy function

--- src/pointestimates/lossfunction/VariationInformation.cpp
+++ src/pointestimates/lossfunction/VariationInformation.cpp
@@ -1,26 +1,36 @@
 #include "VariationInformation.hpp"
-
+#include <set>
 using namespace std;
 
-VariationInformation::VariationInformation(bool normalise_)
-{
+VariationInformation::VariationInformation(bool normalise_) {
   cout << "VariationInformation Constructor" << endl;
   normalise = normalise_;
 }
 
-double VariationInformation::Entropy(Eigen::VectorXi &cluster)
-{
+double VariationInformation::Entropy(Eigen::VectorXi &cluster) {
   double H = 0.0;
-  int K = GetNumberOfGroups(cluster);
+  // int K = GetNumberOfGroups(cluster);
   int nbr = cluster.size();
+  set<int, greater<int>> classes = getClasses(cluster);
 
-  for (int i = 0; i < K; i++)
-  {
+  /*for (auto i : classes) { // Possible optimization --> TODO: test it
     int n = ClassCounter(cluster, i + 1);
+
     double p = (double)n / nbr;
 
     // x*log(x) = 0, if x = 0
-    if (fabs(p) >= 1.0e-9) // ie p != 0
+    if (fabs(p) >= 1.0e-9)  // ie p != 0
+    {
+      H += p * log2(p);
+    }
+  }*/
+  for (int i = 0; i < nbr; i++) {
+    int n = ClassCounter(cluster, i + 1);
+
+    double p = (double)n / nbr;
+
+    // x*log(x) = 0, if x = 0
+    if (fabs(p) >= 1.0e-9)  // ie p != 0
     {
       H += p * log2(p);
     }
@@ -29,17 +39,26 @@ double VariationInformation::Entropy(Eigen::VectorXi &cluster)
   return -H;
 }
 
-double VariationInformation::JointEntropy()
-{
+set<int, greater<int>> getClasses(Eigen::VectorXi &partition) {
+  set<int, greater<int>> s;
+  int size = partition.size();
+
+  for (int i = 0; i < size; i++) {
+    s.insert(partition(i));
+  }
+
+  return s;
+}
+
+double VariationInformation::JointEntropy() {
   double H = 0.0;
-  for (int g = 0; g < K1; g++)
-  {
+  for (int g = 0; g < N; g++) {
     double tmp = 0;
-    for (int h = 0; h < K2; h++)
-    {
-      double p = (double)ClassCounterExtended(*cluster1, *cluster2, 1 + g, 1 + h) / N;
+    for (int h = 0; h < N; h++) {
+      double p =
+          (double)ClassCounterExtended(*cluster1, *cluster2, 1 + g, 1 + h) / N;
       // x*log(x) = 0, if x = 0
-      if (fabs(p) >= 1.0e-9) // ie p != 0
+      if (fabs(p) >= 1.0e-9)  // ie p != 0
       {
         tmp += p * log2(p);
       }
@@ -50,20 +69,14 @@ double VariationInformation::JointEntropy()
   return -H;
 }
 
-double VariationInformation::MutualInformation()
-{
-
+double VariationInformation::MutualInformation() {
   return Entropy(*cluster1) + Entropy(*cluster2) - JointEntropy();
 }
 
-double VariationInformation::Loss()
-{
-  if (!normalise)
-  {
+double VariationInformation::Loss() {
+  if (!normalise) {
     return 2 * JointEntropy() - Entropy(*cluster1) - Entropy(*cluster2);
-  }
-  else
-  {
+  } else {
     return 1 - MutualInformation() / JointEntropy();
   }
 }
